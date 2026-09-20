1c6307009236aaaf60ff49972f551369962aedb3
yerraxd <liliboymak@gmail.com>
2026-09-20 21:26:09 +0500

    random arrays compared with Arrays.sort

--- tests/MergeSorterTest.java
+++ tests/MergeSorterTest.java
@@ -42,5 +42,13 @@ public class MergeSorterTest {
         for (int i = 0; i < arr.length; i++) arr[i] = rnd.nextInt(3);
         check(arr);
     }
-    
+    @Test
+    void randomArrays(){
+        Random rnd = new Random(42);
+        for (int t = 0; t < 100; t++){
+            int[] arr = new int[rnd.nextInt(500)];
+            for (int i = 0; i < arr.length; i++) arr[i] = rnd.nextInt(1000) - 500;
+            check(arr);
+        }
+    }
 }
