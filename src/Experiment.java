1479bb74f770ce25be5fa58e6b114863f9861c5f
yerraxd <liliboymak@gmail.com>
2026-09-20 20:30:46 +0500

    added Experiment and array input generators

---
+++ src/Experiment.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Experiment {
+
+    public static int[] randomArray(int n, long seed) {
+        Random rnd = new Random(seed);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = rnd.nextInt(n * 10 + 1);
+        return arr;
+    }
+
+    public static int[] sortedArray(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = i;
+        return arr;
+    }
+
+    public static int[] reverseSortedArray(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = n - i;
+        return arr;
+    }
+
+    public static int[] duplicateHeavyArray(int n, int distinctValues, long seed) {
+        Random rnd = new Random(seed);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = rnd.nextInt(distinctValues);
+        return arr;
+    }
+}
