a3dddcae876fc1f844df16b1c9dca79a23c5d45a
yerraxd <liliboymak@gmail.com>
2026-09-20 21:31:06 +0500

    add brute-force reference and random comparison (n <= 2000)

--- tests/ClosestPairTest.java
+++ tests/ClosestPairTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
 
 public class ClosestPairTest {
 
@@ -20,5 +21,26 @@ public class ClosestPairTest {
             Point[] pts = Experiment.randomPoints(n, n);
             assertEquals(brute(pts), ClosestPairSolver.closestPair(pts), 1e-9);
         }
+    }
+    @Test
+    void manyEqualXCoordinates(){
+        Random rnd = new Random(5);
+        Point[] pts = new Point[1000];
+        for (int i = 0; i < pts.length; i++){
+            pts[i] = new Point(rnd.nextInt(5), rnd.nextInt(1000));
+        }
+        assertEquals(brute(pts), ClosestPairSolver.closestPair(pts), 1e-9);
+    }
+
+    @Test
+    void duplicatePoints(){
+        Point[] pts = {new Point(1, 1), new Point(5, 5), new Point(1, 1), new Point(9, 2), new Point(7, 7)};
+        assertEquals(0.0, ClosestPairSolver.closestPair(pts), 1e-9);
+    }
+
+    @Test
+    void tooFewPoints(){
+        assertThrows(IllegalArgumentException.class,
+                () -> ClosestPairSolver.closestPair(new Point[]{new Point(0, 0)}));
     }
 }
