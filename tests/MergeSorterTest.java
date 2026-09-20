d53b471931b123783b6c75e6f91ed866ca8846d1
yerraxd <liliboymak@gmail.com>
2026-09-20 21:25:35 +0500

    add sorted, reverse-sorted and duplicate inputs

--- tests/MergeSorterTest.java
+++ tests/MergeSorterTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
 
 public class MergeSorterTest {
 
@@ -20,4 +21,26 @@ public class MergeSorterTest {
     void singleElement(){
         check(new int[]{5});
     }
+    @Test
+    void sortedArray(){
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) arr[i] = i;
+        check(arr);
+    }
+
+    @Test
+    void reverseSortedArray(){
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) arr[i] = 100 - i;
+        check(arr);
+    }
+
+    @Test
+    void manyDuplicates(){
+        Random rnd = new Random(1);
+        int[] arr = new int[200];
+        for (int i = 0; i < arr.length; i++) arr[i] = rnd.nextInt(3);
+        check(arr);
+    }
+    
 }
