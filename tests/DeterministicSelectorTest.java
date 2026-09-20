97115ef6bf4e98217e8d477ac1b40f4ef7b68db8
yerraxd <liliboymak@gmail.com>
2026-09-20 21:30:11 +0500

    small sizes, all-equal values and invalid input

--- tests/DeterministicSelectorTest.java
+++ tests/DeterministicSelectorTest.java
@@ -18,4 +18,30 @@ public class DeterministicSelectorTest {
             assertEquals(sorted[k], DeterministicSelector.select(a.clone(), k));
         }
     }
+    @Test
+    void smallSizesAllK(){
+        Random rnd = new Random(3);
+        for (int n = 1; n <= 12; n++){
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = rnd.nextInt(20);
+            int[] sorted = a.clone();
+            Arrays.sort(sorted);
+            for (int k = 0; k < n; k++){
+                assertEquals(sorted[k], DeterministicSelector.select(a.clone(), k));
+            }
+        }
+    }
+
+    @Test
+    void allEqualValues(){
+        int[] a = new int[10000];
+        Arrays.fill(a, 5);
+        assertEquals(5, DeterministicSelector.select(a, 5000));
+    }
+
+    @Test
+    void invalidInput(){
+        assertThrows(IllegalArgumentException.class, () -> DeterministicSelector.select(new int[0], 0));
+        assertThrows(IllegalArgumentException.class, () -> DeterministicSelector.select(new int[]{1, 2}, 2));
+    }
 }
