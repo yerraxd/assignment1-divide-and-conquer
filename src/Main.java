975329e22ca2f6d3c00cb8f6d8a4c8a3771cedde
yerraxd <liliboymak@gmail.com>
2026-09-20 20:51:16 +0500

    run MergeSort and QuickSort on random input, print to console

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        int[] sizes = {100, 1_000, 10_000};
+        long seed = 42L;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        for (int n : sizes) {
+            int[] random = Experiment.randomArray(n, seed);
+            System.out.println(Experiment.runMergeSort(random, "random"));
+            System.out.println(Experiment.runQuickSort(random, "random"));
+        }
+    }
 }
